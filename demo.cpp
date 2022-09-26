8626149bf25d956b5f5c2c9a9caab6aee3faa23a
duongnhathao001@gmail.com <17121999Hao>
2022-09-26 23:33:18 +0700

    DE

--- demo.cpp
+++ demo.cpp
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include "abc.cpp"
+
+void demo(int a) {
+   printf("%d", a);
+}
+
 int main() {
 
-   char  a = 'asdasdasd';
+   char  a = 'a';
    int q = 1;
    int w = 1;
    int e = 0;
-   // e = w + q;
+   e = w + q;
    w = 3;
    e += w;
    // printf() displays the string inside quotation
-   printf("%d", e);
+   printf("%d\n", e);
+   demo(1);
+   aaaa();
+
    return 0;
 }
