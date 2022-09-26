8730bd20d66730fc4ef0acee89942a4f74af55f5
duongnhathao001@gmail.com <17121999Hao>
2022-09-26 23:16:06 +0700

    demo func

--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,14 @@
 #include <stdio.h>
 int main() {
+
+   char  a = 'asdasdasd';
+   int q = 1;
+   int w = 1;
+   int e = 0;
+   e = w + q;
+   w = 3;
+   e += w;
    // printf() displays the string inside quotation
-   printf("Hello, World!");
+   printf("%d", e);
    return 0;
 }
